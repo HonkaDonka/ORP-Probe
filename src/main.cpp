6e87834b7fa619a6e5d35fe0a26889e4aa5b0193
Vinson Huang <vinsonh789@gmail.com>
2024-01-28 12:37:29 -0600

    IDK

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,12 @@
 
 void setup() {
   Serial.begin(9600);
-
+  // analogReference(EXTERNAL);
   pinMode(WORKINGPROBE, INPUT);
-}
+} 
 
 void loop() {
-  Serial.println(analogRead(WORKINGPROBE));
-  Serial.println("Voltage: " + String(analogRead(WORKINGPROBE) * (5.0 / 1023.0)) + "V");
-  delay(1000);
+  Serial.println(analogRead(WORKINGPROBE) - 512);
+  // Serial.println("Voltage: " + String(analogRead(WORKINGPROBE) * (1.1 / 1023.0)) + "V");
+  delay(500);
 }
