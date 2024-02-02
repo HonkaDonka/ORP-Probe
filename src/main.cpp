67afe0510ca83fc567ba1daa062100e3cad0a7c2
Vinson Huang <vinsonh789@gmail.com>
2024-02-01 22:02:28 -0600

    Woah

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,50 @@
 #include <Arduino.h>
 
-#define WORKINGPROBE A0
+#define ORPPROBE A0
+
+long total = 0;               // Running total of readings
+int times = 0;                // Number of readings
+float average = 0;            // Calculated average value
+
+float vOffset = 0.0;          // Voltage offset
+
+unsigned long previousMillis = 0;
+const long interval = 1000;   // Interval in milliseconds (1 second)
 
 void setup() {
-  Serial.begin(9600);
-  // analogReference(EXTERNAL);
-  pinMode(WORKINGPROBE, INPUT);
-} 
+  Serial.begin(9600);           // Initialize serial communication
+}
 
 void loop() {
-  Serial.println(analogRead(WORKINGPROBE) - 512);
-  // Serial.println("Voltage: " + String(analogRead(WORKINGPROBE) * (1.1 / 1023.0)) + "V");
-  delay(500);
+
+  // Read the analog input
+  int sensorValue = analogRead(ORPPROBE);
+
+  // Add the reading to the total and increment the times variable
+  total += sensorValue;
+  times++;
+
+  unsigned long currentMillis = millis();
+  // Check if the interval has passed
+  if (currentMillis - previousMillis >= interval) {
+    // Save the last time the average was printed
+    previousMillis = currentMillis;
+    // Calculate the average
+    average = total / times;
+    // Print the average to the serial monitor
+    float avgV = average * (5.0 / 1023.0) + vOffset;
+
+    Serial.print("Average Voltage: ");
+    Serial.println(avgV, 2);
+    // Reset the total and times variables
+
+    float calcPPM = 10164 * (sqrt(avgV - 0.38) + 0.0048);
+
+    Serial.print("Calculated PPM: ");
+    Serial.println(calcPPM);
+
+
+    total = 0;
+    times = 0;
+  }
 }
